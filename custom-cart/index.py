bd74172d5a253a59f9a9a9fcd375d04266fecffd
ClaudeRuxpair <165365213+DotBlossom@users.noreply.github.com>
2024-12-09 19:47:16 +0900

    Update index.py

--- custom-cart/index.py
+++ custom-cart/index.py
@@ -1,7 +1,6 @@
 import boto3
 import json
-from botocore.exceptions import ClientError
-import requests
+
 
 client = boto3.client("bedrock-runtime", region_name="ap-northeast-2")
 model_id = "anthropic.claude-3-5-sonnet-20240620-v1:0"  
@@ -10,10 +9,13 @@ def handler(event, context):
     
     try:
         body = json.loads(event['body']) 
-        # 딕셔너리에서 값 가져오기
-        json_input_cart= body["product_metadata_to_str"]  # json.loads() 제거
+
+        json_input_cart= body["custom_cart_data"]
         customCartId = body["custom_cart_id"]
- 
+        
+        cart_data = str(json_input_cart["custom_cart_product_data"])
+        user_data = str(json_input_cart["custom_cart_user_data"])
+        
     except KeyError:
         return {
             'statusCode': 400,
@@ -26,11 +28,13 @@ def handler(event, context):
     # 전체 프롬프트 (줄바꿈 추가)
     user_message = f""" {{json_input_cart}} 에 존재하는 상품들의 이름을 중심으로, 상품들의 메타데이터를 통해 조합을 하여, 이 상품들을 대표하는 하나의 문장(제목)을 작성해줘.
     목표는 이 물품들을 구매하는 사용자에게, 물품 정보를 기반으로 사용자가 왜 이러한 물건들을 샀는지에 대한 하나의 구매내역에 대한 기억할만한 문장(제목)을 남기는거야. 
-     {json_input_user}의 정보 '성별', '나이' 에 맞게 이러한 제목을 작명해줘.
+     {{json_input_user}} 의 정보 '성별', '나이' 에 맞게 이러한 제목을 작명해줘.
 
+    예시 : 막 시작된 겨울을 맞이하는(오늘 날짜가 11월 중순인 경우), 다크 톤의 캐주얼한 드레스 코드(옷의 이름들의 조합으로 추론)
 
-    {{json_input_cart}} : {json_input_cart}
-    {{json_input_user}} : {json_input_user}
+    {{json_input_cart}} : {cart_data}
+    {{json_input_user}} : {user_data}
+    
     """ 
 
 
@@ -57,30 +61,18 @@ def handler(event, context):
 
         response_text = response["output"]["message"]["content"][0]["text"]
         print(response_text)
-        
-        # front 로 결과 전송
-        client_url = "" 
-        api_url = f"{client_url}{customCartId}"  
-        headers = {'Content-Type': 'application/json'}
-        response = requests.post(api_url, headers=headers, data=response_text)
-        
-        # API Gateway 응답 확인
-        if response.status_code == 200:
-            print("API Controller 요청 후 데이터 저장 성공")
-        else:
-            print(f"API Controller 요청 후 처리 실패: {response.status_code}, {response.text}")
 
-     
-    except (ClientError, Exception) as e:
+
+    except (Exception) as e:
         print(f"ERROR: Can't invoke '{model_id}'. Reason: {e}")
         exit(1)
-
 
 
     return {
         'statusCode': 200,
         'body': json.dumps({
-            'response_text': response_text
+            'response_text': response_text,
+            'custom_cart_id' : customCartId
         }),
         'headers': {
              "Content-Type": "application/json"
